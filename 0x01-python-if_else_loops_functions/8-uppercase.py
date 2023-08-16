d3a1295db66f6e732d4f4135d296c7116a16c504
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2023-08-16 11:35:57 +0300

    updated task 8 and 9

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,8 @@
-#!/usr/bin/env python3
-uppercase = __import__('8-uppercase').uppercase
-
-uppercase("best")
-uppercase("Best School 98 Battery street")
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        k = ord(i)
+        if ord(i) >= 97 and ord(i) <= 122:
+            k = k - 32
+        print("{}".format(chr(k)), end="")
+    print("\n")
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-print_last_digit = __import__('9-print_last_digit').print_last_digit
-
-print_last_digit(98)
-print_last_digit(0)
-r = print_last_digit(-1024)
-print(r)
+def print_last_digit(number):
+    if number < 0:
+        number = -number
+    last = number % 10
+    print(last, end="")
+    return l
