2f31b7bb62e6b2619a3b6bdbe644b3f4020935b7
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2023-08-20 14:27:31 +0000

    4th updated matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,4 @@
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for k in i:
-            print("{:d}".format(k), end=" ")
-        print("\n")
+            print("{:d}".format(k), end=" \n")
