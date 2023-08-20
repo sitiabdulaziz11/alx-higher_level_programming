e736e5332dbc642171bb40d7ddbb6f1f6914443e
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2023-08-20 10:06:21 +0000

    updated task

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def element_at(my_list, idx):
     if idx >= len(my_list) or idx < 0:
         return None
--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def no_c(my_string):
     ns = ""
     for i in my_string:
