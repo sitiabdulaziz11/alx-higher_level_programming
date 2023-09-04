0c20a363527da5a51ef0b28466dca85d3419f377
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2023-09-04 22:19:41 +0000

    test file

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
+"""This module is to add two integers
+or two floats
+after casting.
+
+"""
+
 
 def add_integer(a, b=98):
-    """
-    Function that adds two integers.
-
-    @a: first value.
-    @b: second value.
-    return a + b.
-    """
+    """ Function that adds two integers.
+    @a: first value  @b: second value.
+    return a + b."""
 
     if not isinstance(a, int):
         if not isinstance(a, float):
