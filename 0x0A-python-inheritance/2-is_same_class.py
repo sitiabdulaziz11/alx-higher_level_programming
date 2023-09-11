8b01a81de50bdd7906e0b057176dc5a3fc3afd7a
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2023-09-11 20:38:48 +0000

    check obje

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" This module checke objects in the class"""
+
+
+def is_same_class(obj, a_class):
+    """ return true
+    if the object is exactly an instance of the specified class
+    return false otherwise"""
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
