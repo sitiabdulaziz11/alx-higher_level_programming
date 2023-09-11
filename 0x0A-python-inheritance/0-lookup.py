5f642724a3ceec2c92093b101a321343f5e836cd
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2023-09-11 11:34:02 +0000

    return attribute

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module returns list of a variable attribute and
+    method of an object."""
+
+def lookup(obj):
+    """function that returns variable and object"""
+    val = dir(obj)
+    return val
