7a2cf6711370e5d5545b307574ba43887a6762b0
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2023-09-12 07:29:47 +0000

    file handling in python

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,9 +2,9 @@
 """This module is reads a text file"""
 
 
-def read_file(filename=""):
+def read_file(filename="my_file_0.txt"):
     """ Function that read text and print to stdout"""
 
-    with open("my_file_0.txt", 'r') as rd_f:
+    with open(filename, 'r', encoding='utf-8') as rd_f:
         for ln in rd_f:
             print(ln, end="")
