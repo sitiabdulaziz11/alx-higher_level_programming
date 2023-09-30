a9274ba55cfb9ae3c96ba71c240a79955d80e8b3
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2023-09-30 11:31:17 +0000

    updated task 3

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -17,4 +17,4 @@ def say_my_name(first_name, last_name=""):
     if last_name:
         print("My name is {:s} {:s}".format(first_name, last_name))
     else:
-        print("My name is {:s}".format(first_name))
+        print("My name is {:s} ".format(first_name))
