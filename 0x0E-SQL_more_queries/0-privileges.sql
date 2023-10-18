963fd7e2402aef462df5c3a78901bbceee743582
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2023-10-18 08:15:52 -0700

    updated

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,3 @@
+--  script that lists all privileges of the MySQL users user_0d_1
+-- and user_0d_2 on your server (in localhost).
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1'@'localhos';
