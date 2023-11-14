635551744d7c10f58101d3404cbb68319d345e98
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2023-11-14 12:36:27 +0000

    empty object

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,12 +2,10 @@
 
 class Rectangle {
   constructor (w, h) {
-    this.width = w;
-    this.height = h;
-
-    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-      this.width = undefined;
-      this.height = undefined;
+    // if (typeof w === 'number' && typeof h === 'number' && w > 0 && h > 0) {
+    if (Number.isInteger(w) && Number.isInteger(h) && w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
   }
 }
