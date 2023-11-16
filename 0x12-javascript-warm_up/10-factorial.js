7b51c1071ba8e795cb47b46b58f32c7de13900e9
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2023-11-16 09:17:20 +0000

    javascript

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-function factorial(a) {
+function factorial (a) {
   if (a <= 1 || isNaN(a)) {
     return 1;
   }
--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-function add(a, b) {
+function add (a, b) {
   return a + b;
 }
-a = parseInt(process.argv[2]);
-b = parseInt(process.argv[3]);
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
 console.log(add(a, b));
