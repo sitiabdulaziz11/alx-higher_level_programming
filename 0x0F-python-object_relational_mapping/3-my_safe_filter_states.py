48db163bac4cafc373466c11b0b3f8688becf39f
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2023-11-23 15:24:05 +0300

    safe from MySql injections

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+script that takes in arguments and displays all values
+in the states table of hbtn_0e_0_usa where
+name matches the argument.
+and
+that is safe from MySQL injections!
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+
+    try:
+        dbs = MySQLdb.connect(
+                host="localhost",
+                port=3306,
+                user=argv[1],
+                passwd=argv[2],
+                db=argv[3])
+
+        cr = dbs.cursor()
+
+        cr.execute("SELECT * FROM hbtn_0e_0_usa.states \
+                WHERE name LIKE BINARY %s \
+                ORDER BY states.id ASC", (argv[4],))
+
+        for row in cr.fetchall():
+            print(row)
+
+    except MySQLdb.Error as e:
+        print(f"Error: {e}")
+
+    finally:
+        if dbs:
+            dbs.close()
