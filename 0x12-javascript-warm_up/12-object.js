caf2123e94eb8c987240abc7b4e026cbc5e18c12
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2023-12-12 23:39:44 +0300

    mandatory tasks

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+
+// Update the value property of myObject to 89
+myObject.value = 89;
+console.log(myObject);
