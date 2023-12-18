e4aca1f6a2f8932e3825205b1777d5ea5582bd1c
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2023-12-18 16:00:45 +0300

    list object from databases

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+script that lists all State objects from the database hbtn_0e_6_usa
+"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    """
+    Lists all State objects from the database hbtn_0e_6_usa.
+    """
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}' .format(
+        argv[1], argv[2], argv[3]))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).order_by(State.id).all()
+
+    for state in states:
+        print("{}: {}" .format(state.id, state.name))
