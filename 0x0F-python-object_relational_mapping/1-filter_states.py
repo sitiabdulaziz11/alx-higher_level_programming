330c5dffa63c344162426cb3b9aa9f629a1fdbc0
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2023-12-24 17:57:15 +0300

    ufilter states task

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,12 +8,13 @@ import sys
 if __name__ == "__main__":
 
     dbs = MySQLdb.connect(
-            host="localhost", port=3306, user=sys.argv[1],
+            host="localhost", user=sys.argv[1], port=3306,
             passwd=sys.argv[2], db=sys.argv[3])
 
     cur = dbs.cursor()
-    # cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
-    cur.execute("SELECT id, name FROM states WHERE name LIKE '%N' ORDER BY id")
+    # cur.execute("SELECT * FROM states WHERE name LIKE  'N%' ORDER BY id")
+    cur.execute("SELECT * FROM states \
+                WHERE name LIKE BINARY 'N%' ORDER BY states.id ASC")
 
     for rows in cur.fetchall():
         print(rows)
--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+ #!/usr/bin/python3
 """
 script that lists all State objects from the database hbtn_0e_6_usa
 """
