a22d499d043ad0ceadf1c0cd7d33805f7951b8d0
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2024-01-09 12:29:54 +0300

    updated response in body size in bytes

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a request and display the body in byte.
-curl -sI "$1" | grep 'Content-length' | cut -d ' ' -f2
+curl -sI "$1" | grep 'Content-Length' | cut -d ' ' -f2
