91e0229c9445c624356b6a38acad516ba208a7d3
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2024-01-20 12:38:53 +0300

    fetch url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-A Python script that fetches https://alx-intranet.hbtn.io/status
+A Python script that fetches https://alx-intranet.hbtn.io/status.
 """
 
 import urllib.request
