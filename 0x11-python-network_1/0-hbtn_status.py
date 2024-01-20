2c552aa8a0c0038d71254e9b8a012d640962ad7e
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2024-01-20 14:15:28 +0300

    2nd updated url fetch

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,9 +5,10 @@ A Python script that fetches https://alx-intranet.hbtn.io/status.
 
 import urllib.request
 
-url = "https://alx-intranet.hbtn.io/status"
 
 if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+
     with urllib.request.urlopen(url) as respo:
         html_cont = respo.read()
         deco_cont = html_cont.decode('utf-8')
