7226a1670168720473f51f8b188fb1ea23db630c
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2024-01-20 14:08:46 +0300

    updated fetch url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,11 +7,12 @@ import urllib.request
 
 url = "https://alx-intranet.hbtn.io/status"
 
-with urllib.request.urlopen(url) as respo:
-    html_cont = respo.read()
-    deco_cont = html_cont.decode('utf-8')
+if __name__ == "__main__":
+    with urllib.request.urlopen(url) as respo:
+        html_cont = respo.read()
+        deco_cont = html_cont.decode('utf-8')
 
-print("Body response:")
-print("\t- type: {}".format(type(html_cont)))
-print("\t- content: {}".format(html_cont))
-print("\t- utf8 content: {}".format(deco_cont))
+    print("Body response:")
+    print("\t- type: {}".format(type(html_cont)))
+    print("\t- content: {}".format(html_cont))
+    print("\t- utf8 content: {}".format(deco_cont))
